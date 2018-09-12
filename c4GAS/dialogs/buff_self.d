fe6cccefb2f5c27e9b38669b3c6cc2d07e033816
c4_angel <20829444@qq.com>
2018-09-12 09:37:55 +0800

    v1.0

---
+++ c4GAS/dialogs/buff_self.d
@@ -0,0 +1,26 @@
+
+ALTER_TRANS c4AIcons
+BEGIN 7 END
+BEGIN ~%trans%~ END
+BEGIN
+"TRIGGER" ~Global("%trans_var%","Global",0)~
+END
+
+EXTEND_TOP ~c4AIcons~ ~%state%~
+  ++ ~%spell_name%~ + set
+  // + ~HaveKnownSpellRES("%spell%")~ + ~%spell_name%~ + set
+END
+
+
+APPEND ~c4AIcons~
+
+IF ~~ THEN BEGIN set
+	SAY #-1
+	+ ~!Global("c4AI_Buff_%spell%","Locals",%default%)~ + @41
+		DO ~SetGlobal("c4AI_Buff_%spell%","Locals",%default%)~ FLAGS 512 + ~%state%~
+	+ ~!Global("c4AI_Buff_%spell%","Locals", -1)~ + @42
+		DO ~SetGlobal("c4AI_Buff_%spell%","Locals", -1)~ FLAGS 512 + ~%state%~
+	++ @2 GOTO ~%state%~
+END
+
+END
